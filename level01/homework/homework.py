0af0d097eb9c2486a3b0f851843fd29ff809b7d5
davitkacitadze <vip.kokakola@gmail.com>
2024-10-11 19:02:47 +0400

    Update homework.py

---
+++ level01/homework/homework.py
@@ -0,0 +1,33 @@
+name= "dato"
+#name არის ცვლადი
+# = არის ცვლადისთვის მიმნიჭებელი სიმბოლო
+# "dato" არის ცვლადისთვის მნიშვნელობა
+
+surname= "kacitadze"
+
+#print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name= "dato" # ეს არის str(string) ტიპის ცვლადი
+age = 17 #ეს არის int( integer ) მთელი რიცხვი
+height = 165 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი
+
+knows_programming = True #true or false
+is_ugly = False #snakecase( უბრალოდ წერის სტილი სტანდარტულად )
+
+is_ugly =False # ჯავასკრიპტული camelcase
+
+
+#print(name + " " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები 
+##print(name + age)
+
+# print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print(name + " " + str(age))
